b19a550449ad0bcf5a184076fc9881bccd02284b
mtrimner <matt.trimner@gmail.com>
2020-12-17 09:48:06 -0800

    commit tree 09e4bad81570a403d514a77760cb9e4296d54043

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
-// Code Keypad Component Here
+import React, { Component } from 'react';
+
+class Keypad extends Component {
+
+    handleInput = () => {
+        console.log('Entering password...')
+    }
+    render() {
+        return (
+            <div>
+                <input type="password" onKeyUp={this.handleInput}/>
+            </div>
+        );
+    }
+}
+
+export default Keypad;
